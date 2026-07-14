0403b39152a4b51c8db85556de715682d36fa548
umutthyke-pixel <umutthyke-pixel@users.noreply.github.com>
2026-07-14 15:45:25 +0300

    Clean up local debug scripts

--- check.js
+++
@@ -1,8 +0,0 @@
-var htmlfile = new ActiveXObject('htmlfile');
-htmlfile.write('<meta http-equiv=\"x-ua-compatible\" content=\"IE=11\" /><script>window.localStorage = { getItem: function(k){return null;}, setItem: function(k,v){} };</script><script src=\"file:///' + 'C:/Users/umut/Desktop/rastgele-site/index.html'.replace(/\\\\/g, '/') + '\"></script>');
-var win = htmlfile.parentWindow;
-try {
-    WScript.Echo('factsDB type is: ' + typeof win.factsDB);
-} catch(e) {
-    WScript.Echo('Error: ' + e.message);
-}
--- test.js
+++
@@ -1,9 +0,0 @@
-var htmlfile = new ActiveXObject('htmlfile');
-htmlfile.write('<meta http-equiv=\"x-ua-compatible\" content=\"IE=11\" /><script>window.localStorage = { getItem: function(k){return null;}, setItem: function(k,v){} };</script><script src=\"file:///' + 'C:/Users/umut/Desktop/rastgele-site/debug.js'.replace(/\\\\/g, '/') + '\"></script>');
-var win = htmlfile.parentWindow;
-try {
-    win.eval('nextFact();');
-    WScript.Echo('nextFact ran successfully');
-} catch(e) {
-    WScript.Echo('Error: ' + e.message);
-}
